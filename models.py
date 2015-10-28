b0d34104e62ce1897bfaf9ffc580d8f217539aeb
Raymond Jacobson <me@raymondjacobson.com>
2015-10-28 09:33:47 -0400

    Lint Python server code with flake8

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import datetime
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,133 +7,141 @@ db = client['c4']
 games_collection = db['games']
 players_collection = db['players']
 
+
 def enum(**enums):
     return type('Enum', (), enums)
 
 PlayerTypes = enum(SPECTATOR='spectator', HOST='host', CHALLENGER='challenger')
 
+
 class Game():
-  """
-  This class wraps the MongoDB game object
-  Schema:
-    - name
-    - host_id
-    - host_name
-    - board
-    - host_turn
-    - last_access
-    - challenger
-    - staged_delete (used to confirm a game delete from both players when 
-                     a game ends)
-  """
-  @classmethod
-  def new(self, name, host_id, host_name):
-    if name == "":
-      name = host_name + "'s game"
-    _id = games_collection.insert_one({
-      "name": name,
-      "host_id": host_id,
-      "host_name": host_name,
-      "board": Board.new(),
-      "host_turn": True,
-      "last_access": datetime.datetime.utcnow()
-    }).inserted_id
-    return _id
+    '''
+    This class wraps the MongoDB game object
+    Schema:
+      - name
+      - host_id
+      - host_name
+      - board
+      - host_turn
+      - last_access
+      - challenger
+      - staged_delete (used to confirm a game delete from both players when
+                       a game ends)
+    '''
+    @classmethod
+    def new(cls, name, host_id, host_name):
+        if name == "":
+            name = host_name + "'s game"
+        _id = games_collection.insert_one({
+            "name": name,
+            "host_id": host_id,
+            "host_name": host_name,
+            "board": Board.new(),
+            "host_turn": True,
+            "last_access": datetime.datetime.utcnow()
+        }).inserted_id
+        return _id
 
-  @classmethod
-  def get(self, _id):
-    return games_collection.find_one({"_id": ObjectId(_id)})
+    @classmethod
+    def get(cls, _id):
+        return games_collection.find_one({"_id": ObjectId(_id)})
 
-  @classmethod
-  def drop(self, _id):
-    games_collection.delete_one({"_id": ObjectId(_id)})
+    @classmethod
+    def drop(cls, _id):
+        games_collection.delete_one({"_id": ObjectId(_id)})
 
-  @classmethod
-  def staged_drop(self, _id):
-    # Stage the game for a drop, but wait for confirmation from both players
-    # to actually drop it
-    game = self.get(_id)
-    if 'staged_delete' in game.keys():
-      games_collection.delete_one({"_id": ObjectId(_id)})
-    else:
-      games_collection.update_one({"_id": ObjectId(_id)},
-                                  {"$set": {"staged_delete": True} })
+    @classmethod
+    def staged_drop(cls, _id):
+        # Stage the game for a drop, but wait for confirmation
+        # from both players to actually drop it
+        game = cls.get(_id)
+        if 'staged_delete' in game.keys():
+            games_collection.delete_one({"_id": ObjectId(_id)})
+        else:
+            games_collection.update_one({"_id": ObjectId(_id)},
+                                        {"$set": {"staged_delete": True}})
 
-  @classmethod
-  def get_games(self):
-    return games_collection.find()
+    @classmethod
+    def get_games(cls):
+        return games_collection.find()
 
-  @classmethod
-  def set_challenger(self, _id, challenger):
-    games_collection.update_one({"_id": ObjectId(_id)},
-                                {"$set": {"challenger": challenger} })
+    @classmethod
+    def set_challenger(cls, _id, challenger):
+        games_collection.update_one({"_id": ObjectId(_id)},
+                                    {"$set": {"challenger": challenger}})
 
-  @classmethod
-  def set_board(self, _id, board, host_turn):
-    games_collection.update_one({"_id": ObjectId(_id)},
-                                {"$set": {"board": board,
-                                          "host_turn": not host_turn,
-                                          "last_access": datetime.datetime.utcnow()} })
+    @classmethod
+    def set_board(cls, _id, board, host_turn):
+        games_collection.update_one({"_id": ObjectId(_id)},
+                                    {"$set": {"board": board,
+                                              "host_turn": not host_turn,
+                                              "last_access":
+                                              datetime.datetime.utcnow()}})
 
-  @classmethod
-  def score(self, _id):
-    game = games_collection.find_one({"_id": ObjectId(_id)})
-    if game:
-      board = game['board']
-      return Board.score(board)
-    return 0
+    @classmethod
+    def score(cls, _id):
+        game = games_collection.find_one({"_id": ObjectId(_id)})
+        if game:
+            board = game['board']
+            return Board.score(board)
+        return 0
 
 
 class Board():
-  """
-  This class wraps the MongoDB board object, stored as a 6x7 array
-  """
-  @classmethod
-  def new(self):
-    return [ [0 for x in xrange(7)] for y in xrange(6) ]
+    '''
+    This class wraps the MongoDB board object, stored as a 6x7 array
+    '''
+    @classmethod
+    def new(cls):
+        return [[0 for x in xrange(7)] for y in xrange(6)]
 
-  @classmethod
-  def update(self, state, column, pid):
-    column = int(column)
-    for r in xrange(len(state)-1, -1, -1):
-      if state[r][column] == 0:
-        state[r][column] = pid
-        break
-    return state
+    @classmethod
+    def update(cls, state, column, pid):
+        column = int(column)
+        for r in xrange(len(state) - 1, -1, -1):
+            if state[r][column] == 0:
+                state[r][column] = pid
+                break
+        return state
 
-  @classmethod
-  def score(self, state):
-    # Score the rows
-    for row in xrange(len(state)):
-      for col in xrange(3, len(state[row])):
-        if 0 != state[row][col-3] == state[row][col-2] == state[row][col-1] == state[row][col]:
-          return state[row][col]
+    @classmethod
+    def score(cls, state):
+        # Score the rows
+        for row in xrange(len(state)):
+            for col in xrange(3, len(state[row])):
+                if 0 != state[row][col - 3] == state[row][col - 2] == \
+                   state[row][col - 1] == state[row][col]:
+                    return state[row][col]
 
-    # Score the columns
-    for col in xrange(len(state[0])):
-      for row in xrange(3, len(state)):
-        if 0 != state[row-3][col] == state[row-2][col] == state[row-1][col] == state[row][col]:
-          return state[row][col]
+        # Score the columns
+        for col in xrange(len(state[0])):
+            for row in xrange(3, len(state)):
+                if 0 != state[row - 3][col] == state[row - 2][col] == \
+                   state[row - 1][col] == state[row][col]:
+                    return state[row][col]
 
-    # Score the diagonals
-    for row in xrange(len(state)-4, -1, -1):
-      for col in xrange(3, len(state[row])):
-        if 0 != state[row+3][col-3] == state[row+2][col-2] == state[row+1][col-1] == state[row][col]:
-          return state[row][col]
-        if 0 != state[row+3][col] == state[row+2][col-1] == state[row+1][col-2] == state[row][col-3]:
-          return state[row+3][col]
-    return 0
+        # Score the diagonals
+        for row in xrange(len(state) - 4, -1, -1):
+            for col in xrange(3, len(state[row])):
+                if 0 != state[row + 3][col - 3] == state[row + 2][col - 2] == \
+                   state[row + 1][col - 1] == state[row][col]:
+                    return state[row][col]
+                if 0 != state[row + 3][col] == state[row + 2][col - 1] == \
+                   state[row + 1][col - 2] == state[row][col - 3]:
+                    return state[row + 3][col]
+        return 0
+
 
 class Player():
-  """
-  This class wraps the MongoDB player object
-  Maintains a name and unique ID
-  """
-  @classmethod
-  def new(self, name):
-    _id = players_collection.insert_one({"name":name}).inserted_id
-    return _id
+    '''
+    This class wraps the MongoDB player object
+    Maintains a name and unique ID
+    '''
+    @classmethod
+    def new(cls, name):
+        _id = players_collection.insert_one({"name": name}).inserted_id
+        return _id
 
-  @classmethod
-  def load(self, _id):
-    return players_collection.find_one({"_id":ObjectId(_id)})
+    @classmethod
+    def load(cls, _id):
+        return players_collection.find_one({"_id": ObjectId(_id)})
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, redirect, render_template, request, session, url_for
+from flask import Flask, jsonify, redirect, render_template, request, session, \
+    url_for
 from flask.ext.session import Session
 
 from middleware import ReverseProxied
@@ -11,113 +12,123 @@ SESSION_TYPE = 'redis'
 app.config.from_object(__name__)
 Session(app)
 
+
 @app.route("/", methods=['GET'])
 def index():
-  # Check if the visitor is a current valid user
-  if '_id' in session.keys() and Player.load(session['_id']):
-    player = Player.load(session['_id'])
-    games = list(Game.get_games())
-    return render_template('index.html',
-                            title="C4 4 PG",
-                            games=games,
-                            player=player)
-  else:
-    return render_template('new_player.html',
-                           title="C4 4 PG | new player",
-                           destination=url_for('index'))
+    # Check if the visitor is a current valid user
+    if '_id' in session.keys() and Player.load(session['_id']):
+        player = Player.load(session['_id'])
+        games = list(Game.get_games())
+        return render_template('index.html',
+                               title="C4 4 PG",
+                               games=games,
+                               player=player)
+    else:
+        return render_template('new_player.html',
+                               title="C4 4 PG | new player",
+                               destination=url_for('index'))
+
 
 @app.route("/admin", methods=['POST'])
 def admin():
-  p_id = Player.new(request.form['name'])
-  session['_id'] = p_id
-  return redirect(request.form['destination'])
+    p_id = Player.new(request.form['name'])
+    session['_id'] = p_id
+    return redirect(request.form['destination'])
+
 
 @app.route("/game/new", methods=['POST'])
 def new_game():
-  player = Player.load(session['_id'])
-  game_id = Game.new(request.form['name'], player['_id'], player['name'])
-  return redirect("game/"+str(game_id))
+    player = Player.load(session['_id'])
+    game_id = Game.new(request.form['name'], player['_id'], player['name'])
+    return redirect("game/" + str(game_id))
+
 
 @app.route('/game/<game_id>/delete', methods=['POST'])
 def delete_game(game_id):
-  Game.drop(game_id)
-  return redirect(url_for('index'))
+    Game.drop(game_id)
+    return redirect(url_for('index'))
+
 
 @app.route('/game/<game_id>/joint_delete', methods=['POST'])
 def joint_delete_game(game_id):
-  Game.staged_drop(game_id)
-  return redirect(url_for('index'))
+    Game.staged_drop(game_id)
+    return redirect(url_for('index'))
+
 
 @app.route("/game/<game_id>", methods=['GET'])
 def view_game(game_id):
-  if '_id' in session.keys() and Player.load(session['_id']):
-    player = Player.load(session['_id'])
-    game = Game.get(game_id)
-    if not game:
-      return redirect(url_for('index'))
-    winner = Game.score(game_id)
-    player_type = PlayerTypes.HOST
-    if game['host_id'] != player['_id']:
-      player_type = PlayerTypes.CHALLENGER
-      # The first visitor to the game is the challenger
-      if 'challenger' not in game.keys():
-        Game.set_challenger(game_id, player['_id'])
+    if '_id' in session.keys() and Player.load(session['_id']):
+        player = Player.load(session['_id'])
         game = Game.get(game_id)
-      # Render a tempalate for a spectator if they're not either of the players
-      if game['challenger'] != player['_id']:
-        player_type = PlayerTypes.SPECTATOR
-    return render_template('game.html',
-                            title="C4 4 PG | " + game_id,
-                            game=game,
-                            player=player,
-                            winner=winner,
-                            player_type=player_type)
-  else:
-    return render_template('new_player.html',
-                           title="C4 4 PG | new player",
-                           destination=url_for('view_game', game_id=game_id))
+        if not game:
+            return redirect(url_for('index'))
+        winner = Game.score(game_id)
+        player_type = PlayerTypes.HOST
+        if game['host_id'] != player['_id']:
+            player_type = PlayerTypes.CHALLENGER
+            # The first visitor to the game is the challenger
+            if 'challenger' not in game.keys():
+                Game.set_challenger(game_id, player['_id'])
+                game = Game.get(game_id)
+            # Render a tempalate for a spectator if they're
+            # not either of the players
+            if game['challenger'] != player['_id']:
+                player_type = PlayerTypes.SPECTATOR
+        return render_template('game.html',
+                               title="C4 4 PG | " + game_id,
+                               game=game,
+                               player=player,
+                               winner=winner,
+                               player_type=player_type)
+    else:
+        return render_template('new_player.html',
+                               title="C4 4 PG | new player",
+                               destination=url_for('view_game',
+                                                   game_id=game_id))
+
 
 @app.route("/game/<game_id>/board", methods=['GET'])
 def load_board(game_id):
-  game = Game.get(game_id)
-  if not game:
-    return jsonify(active=False)
-  winner = Game.score(game_id)
-  host_turn = game['host_turn']
-  host_name = game['host_name']
-  board = game['board']
-  if 'challenger' in game.keys():
-    challenger_name = Player.load(game['challenger'])['name']
-  else:
-    challenger_name = "Waiting for a challenger"
-  return jsonify(active=True,
-                 board=board,
-                 winner=winner,
-                 host_turn=host_turn,
-                 host_name=host_name,
-                 challenger_name=challenger_name)
+    game = Game.get(game_id)
+    if not game:
+        return jsonify(active=False)
+    winner = Game.score(game_id)
+    host_turn = game['host_turn']
+    host_name = game['host_name']
+    board = game['board']
+    if 'challenger' in game.keys():
+        challenger_name = Player.load(game['challenger'])['name']
+    else:
+        challenger_name = "Waiting for a challenger"
+    return jsonify(active=True,
+                   board=board,
+                   winner=winner,
+                   host_turn=host_turn,
+                   host_name=host_name,
+                   challenger_name=challenger_name)
+
 
 @app.route("/move", methods=['POST'])
 def move():
-  p = Player.load(session['_id'])
-  game_id = request.form['game_id']
-  move_location = request.form['move_location']
+    p = Player.load(session['_id'])
+    game_id = request.form['game_id']
+    move_location = request.form['move_location']
 
-  # Get the current board and update it with the new move
-  game = Game.get(game_id)
-  board = game['board']
-  # Check which player to place the token for and check
-  # that the player has the current board move
-  if game['host_id'] == p['_id'] and game['host_turn']:
-    new_board = Board.update(board, move_location, PlayerTypes.HOST)
-    Game.set_board(game_id, new_board, True)
-  elif 'challenger' in game.keys() and \
-    game['challenger'] == p['_id'] and not game['host_turn']:
-    
-    new_board = Board.update(board, move_location, PlayerTypes.CHALLENGER)
-    Game.set_board(game_id, new_board, False)
+    # Get the current board and update it with the new move
+    game = Game.get(game_id)
+    board = game['board']
+    # Check which player to place the token for and check
+    # that the player has the current board move
+    if game['host_id'] == p['_id'] and game['host_turn']:
+        new_board = Board.update(board, move_location, PlayerTypes.HOST)
+        Game.set_board(game_id, new_board, True)
+    elif 'challenger' in game.keys() and \
+         game['challenger'] == p['_id'] and not game['host_turn']:
 
-  return redirect("game/"+str(game_id))
+        new_board = Board.update(board, move_location, PlayerTypes.CHALLENGER)
+        Game.set_board(game_id, new_board, False)
+
+    return redirect("game/" + str(game_id))
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
